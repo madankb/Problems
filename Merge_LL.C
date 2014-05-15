1700ba9e71a4c8089e97648a6406b00b6be6ec08
MadanKumarB <madankumar.baskaran@gmail.com>
2014-05-15 14:29:43 +0530

    Update Merge_LL.C

--- Merge_LL.C
+++ Merge_LL.C
@@ -22,6 +22,15 @@ void insert(struct node** head,int key)
      *head=temp;
    }
 }
+int size(struct node* list)
+{
+  int i=0;
+  while (list != NULL)
+  {
+    i++;
+    list=list->next;
+  }
+}
 struct node* merge(struct node* front,struct node* back)
 {
   if (front == NULL && back == NULL)
@@ -33,7 +42,7 @@ struct node* merge(struct node* front,struct node* back)
   if (back == NULL)
   return front;
 
-  cout<<"Front and Back key is :"<<front->key<<"  "<<back->key<<endl;
+  //cout<<"Front and Back key is :"<<front->key<<" "<<back->key<<endl;
   if (front->key < back->key)
   {
      front->next=merge(front->next,back);
@@ -53,6 +62,51 @@ void print(struct node* p)
      p=p->next;
    }
    cout<<endl;
+}
+struct node* split(struct node* list,int size)
+{
+   if (list->next == NULL)
+   return list;
+
+   if (list == NULL) 
+   return NULL;
+
+   struct node* front=list;
+   struct node* temp=list;
+   struct node* back=NULL;
+
+   int length=0;
+   int half=size/2;
+
+   while (length != half)
+   {
+       temp=temp->next;
+       length=length+1;
+   }
+   
+   back=temp->next;
+   temp->next=NULL;
+
+   front=split(front,half);
+   back=split(back,half);
+  
+   list=merge(front,back);
+      
+   return list; 
+}
+struct node* mergesort(struct node* list)
+{
+    printf("Before Merge sort : \n");
+    
+    print(list);
+    
+    list=split(list,size(list));
+    
+    printf("After Merge sort : \n");
+    
+    print(list);
+    
+    return list;
 }
 int main()
 {
@@ -75,5 +129,14 @@ int main()
    head=merge(head,tail);
 
    print(head);
+
+   struct node* a=NULL;
+   insert(&a,1);
+   insert(&a,32);
+   insert(&a,56);
+   insert(&a,12);
+   insert(&a,46);
+   
+   a=mergesort(a);
 
 }
