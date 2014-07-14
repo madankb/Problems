750e5092e3dba4f653d99544381bf4da3929bba9
MadanKumarB <madankumar.baskaran@gmail.com>
2014-07-14 11:41:36 +0530

    Update No_of_BST.C

--- No_of_BST.C
+++ No_of_BST.C
@@ -3,24 +3,24 @@
 using namespace std;
 int No_of_BST(int A[],int start,int end)
 {
-  if (start > end)
-     return 0;
-
- if (start == end)
+ if (start >= end)
     return 1;
 
- int max=0;
+ int sum=0;
 
  for(int i=start;i<=end;i++)
  {
-    max=max+No_of_BST(A,start,i-1)+No_of_BST(A,i+1,end);
+    int left=No_of_BST(A,start,i-1);
+    int right=No_of_BST(A,i+1,end);
+  
+   sum = sum + (left*right);
  }
 
- return max;
+ return sum;
 }
 int main()
 {
-  int A[]={1,2};
+  int A[]={1,2,3,4};
   int size=sizeof(A)/sizeof(A[0]);
   
   cout<<"Number of BST is : "<<No_of_BST(A,0,size-1)<<endl;
